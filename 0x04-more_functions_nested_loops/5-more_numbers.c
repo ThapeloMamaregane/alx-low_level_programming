b278f69c9fa6d6fb6fe33be910836a0745821572
ThapeloMamaregane <chrismamaregane@gmail.com>
2023-06-22 12:25:03 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,9 +14,11 @@ int i, j;
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
-			_putchar('1');
-			_putchar(j % 10 + '0');
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
 		}
-			_putchar('\n');
-		}
+		_putchar('\n');
+	}
 }
