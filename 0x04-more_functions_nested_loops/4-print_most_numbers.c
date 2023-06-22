897fa6a65f5c1049d46b176bad8fb2ee1bb4af78
ThapeloMamaregane <chrismamaregane@gmail.com>
2023-06-22 07:11:42 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,12 @@
 
 void print_most_numbers(void)
 {
-	char num;
+	char c;
 
-	for (num = '0'; num <= '9'; num++)
+	for (c = '0'; c <= '9'; c++)
 	{
-	if (!(num == '2' || num == '4'))
-	_putchar(num);
+	if (!(c == '2' || c == '4'))
+	_putchar(c);
 	}
 	_putchar('\n');
 }
