88d4b08bf4e56a549b4b97cc9bcc91f470936302
ThapeloMamaregane <chrismamaregane@gmail.com>
2023-06-29 06:56:41 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string in 1337
+ * @s: string to be encoded
+ *
+ * Return: the resulting string;
+ */
+char *leet(char *n)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == a[j])
+			{
+				n[i] = b[j];
+			}
+		}
+	}
+
+	return (n);
+}
