ba59047c5cf39a35ff703a1440371804be6ba478
ThapeloMamaregane <chrismamaregane@gmail.com>
2023-07-06 09:58:49 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 
-int _putchar(char);
+int _putchar.c(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
