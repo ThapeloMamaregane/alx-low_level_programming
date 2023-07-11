7f89cce692fde15353451c80c699e1dfd39b47a6
ThapeloMamaregane <chrismamaregane@gmail.com>
2023-07-11 13:40:30 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - Entry point
- *
- * @argc: incoming argument
- * @argv: incoming argument
- *
- * Return: Always 0 (Success)
+ * main - function to print multiplication result
+ * @argc: number of arguments
+ * @argv: arguments supplied
+ * Return: always 1 if less than two arg var supplied otherwise 0.
  */
 int main(int argc, char **argv)
+{
+	int i = 0, j = 0;
+
+	if (argc > 2)
 	{
-
-	 int sum;
-
-	if (argc < 2)
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		printf("%d\n", i * j);
+	}
+	else
 	{
 		printf("Error\n");
-		return (1);
 	}
-
-	sum = (int) *argv[1] * (int) *argv[2];
-
-	printf("%d\n", sum);
-
 	return (0);
-
-	}
+}
