c6c747a466157f43bb6839f5c98540028e425bc4
ThapeloMamaregane <chrismamaregane@gmail.com>
2023-07-30 10:36:18 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -33,7 +33,7 @@ int main(void)
 		printf(", %lu", c1 + (c2 / 1000000000));
 		printf("%lu", c2 % 1000000000);
 		c1 = c1 + b1;
-		v1 = c1 - b1;
+		c1 = c1 - b1;
 		c2 = c2 + b2;
 		c2 = c2 - b2;
 	}
