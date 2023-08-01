28b0bb3d55a3592f9b319d07fa0239028fd69408
ThapeloMamaregane <chrismamaregane@gmail.com>
2023-08-01 06:36:36 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,30 +8,30 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int i = 0;
-	listint_t *tmp_h, *new_node;
+	unsigned int ctr = 0;
+	listint_t *tmp_h, *tmp_node;
 
 	if (!*head)
 		return (-1);
 	tmp_h = *head;
 	if (index == 0)
 	{
-		new_node = tmp_h->next;
-		*head = new_node;
+		tmp_node = tmp_h->next;
+		*head = tmp_node;
 		free(tmp_h);
 		return (1);
 	}
 	while (tmp_h)
 	{
-		if (i + 1 == index)
+		if (ctr + 1 == index)
 		{
-			new_node = tmp_h->next;
-			tmp_h->next = new_node->next;
+			tmp_node = tmp_h->next;
+			tmp_h->next = tmp_node->next;
 			free(tmp_node);
 			return (1);
 		}
 		tmp_h = tmp_h->next;
-		i++;
+		ctr++;
 	}
 	return (-1);
 }
