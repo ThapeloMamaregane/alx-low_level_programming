c17be7ba888a27915ca55800c68a6d8f72e08396
ThapeloMamaregane <chrismamaregane@gmail.com>
2023-08-07 12:59:11 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,18 +13,18 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	new_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (new_file == -1)
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
 		return (-1);
 	if (text_content == NULL)
 	{
-		close(new_file);
+		close(file);
 		return (1);
 	}
-	for (length = 0; text_content[len]; length++)
+	for (length = 0; text_content[length]; length++)
 		;
 	status = write(new_file, text_content, length);
-	if (close(new_file) == -1)
+	if (close(file) == -1)
 		return (-1);
 	return (status == -1 ? -1 : 1);
 }
